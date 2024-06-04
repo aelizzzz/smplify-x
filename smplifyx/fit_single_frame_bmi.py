34201b5030037895905f132b22bcaf3ef5ba7b41
aelizzzz <158032024+aelizzzz@users.noreply.github.com>
2024-06-04 17:22:02 +0200

    Updated bmi version to fit tailang's changes for meshcapade

--- smplifyx/fit_single_frame_bmi.py
+++ smplifyx/fit_single_frame_bmi.py
@@ -17,7 +17,6 @@
 
 # Modified to take into account fixed height and weight information before 
 # optimisation of the model
-# Modified to return the 3D position of the regressed joints after fitting
 
 from __future__ import absolute_import
 from __future__ import print_function
@@ -75,7 +74,7 @@ def measurements_to_betas(height, weight, gender):
     return np.transpose(betas)
 
 
-def fit_single_frame(img,
+def fit_single_frame_bmi(img,
                      keypoints,
                      height,
                      weight,
@@ -124,7 +123,6 @@ def fit_single_frame(img,
                      interactive=True,
                      visualize=False,
                      save_meshes=True,
-                     save_joints=True,
                      degrees=None,
                      batch_size=1,
                      dtype=torch.float32,
@@ -422,7 +420,7 @@ def fit_single_frame(img,
 
         # Step 2: Optimize the full model
         final_loss_val = 0
-        for or_idx, orient in enumerate(tqdm(orientations, desc='Orientbody_modelation')):
+        for or_idx, orient in enumerate(tqdm(orientations, desc='Orientation')):
             opt_start = time.time()
 
             betas_bmi = torch.tensor(measurements_to_betas(height, weight, gender), dtype=torch.float32)
@@ -435,7 +433,7 @@ def fit_single_frame(img,
             if use_vposer:
                 with torch.no_grad():
                     pose_embedding.fill_(0)
-            
+
             body_model.betas.requires_grad = False
             for opt_idx, curr_weights in enumerate(tqdm(opt_weights, desc='Stage')):
 
@@ -508,8 +506,13 @@ def fit_single_frame(img,
             result.update({key: val.detach().cpu().numpy()
                            for key, val in body_model.named_parameters()})
             if use_vposer:
-                result['body_pose'] = pose_embedding.detach().cpu().numpy()
-
+                # Below vpose.decode is for MPG's (and meshcapade github master) add-on to correctly load 'pkl' file, target shape is (NUM_JOINTS,3)
+                result['body_pose'] = vposer.decode(pose_embedding, output_type='aa').detach().cpu().numpy().reshape((1,63))
+                # Original code
+                # result['body_pose'] = pose_embedding.detach().cpu().numpy()
+            # Set 'global_orient' to zero while keeping its shape for saving pkl
+            # TODO: we need to save global_orient in other form for later use in simulation
+            result['global_orient'] = np.zeros_like(result['global_orient'])
             results.append({'loss': final_loss_val,
                             'result': result})
 
@@ -522,9 +525,17 @@ def fit_single_frame(img,
             pickle.dump(results[min_idx]['result'], result_file, protocol=2)
 
     if save_meshes or visualize:
+        # vpose.decode for the pkl above does not share here, so have to call it again
         body_pose = vposer.decode(
             pose_embedding,
             output_type='aa').view(1, -1) if use_vposer else None
+        # If need to get the joints with T posed, otherwise have to load standing pose first
+        # Which means no prediction is expected, rather just get the joints coordinates from default pose
+        # No need to reset pose in pkl above, since meaningless
+        # If need to use results from prediction, comment below
+        #body_pose = torch.zeros_like(body_pose, device=body_pose.device, requires_grad=True)
+        #body_model.reset_params(body_pose=body_pose)
+
 
         model_type = kwargs.get('model_type', 'smpl')
         append_wrists = model_type == 'smpl' and use_vposer
@@ -534,15 +545,39 @@ def fit_single_frame(img,
                                          device=body_pose.device)
                 body_pose = torch.cat([body_pose, wrist_pose], dim=1)
 
+        # above we set the result's global_orient for saving pkl, but here we need to set to body_model for regression joints
+        # set the body pose tensor to None so no local rotations are applied, for saving joint coordinates
+        global_orient= torch.zeros(1, 3, device='cuda:0', requires_grad=True)
+        body_model.reset_params(global_orient=global_orient)
+        # here if want to save the joints in T pose, set body_pose to None
+        # or for standing pose, set body_pose to standing_pose
         model_output = body_model(return_verts=True, body_pose=body_pose)
+        # Get the joints coordinates from the model
+        joints = model_output.joints.detach().cpu().numpy().squeeze()
+        joints *= 1000 # convert to mm
+        # rotate joints around x-axis for +90 degree
+        import trimesh
+        rot = trimesh.transformations.rotation_matrix(
+            np.radians(90), [1, 0, 0])
+        joints = trimesh.transform_points(joints, rot)
+        # Just for checking if the global_orient is reset
+        print("body_model's orient: ", body_model.global_orient)
+        #save joints as json
+        import json
+        with open(joints_path, 'w') as f:
+            json.dump(joints.tolist(), f)
+
         vertices = model_output.vertices.detach().cpu().numpy().squeeze()
 
-        import trimesh
+        #import trimesh
 
         out_mesh = trimesh.Trimesh(vertices, body_model.faces, process=False)
         rot = trimesh.transformations.rotation_matrix(
-            np.radians(180), [1, 0, 0])
-        out_mesh.apply_transform(rot)
+            np.radians(180), [1, 0, 0]) # Why rotate 180 degrees around x-axis?
+        # Removed scaling of the mesh output, it breaks SMPLX to SMPL and then OSSO
+        #scale_transform = trimesh.transformations.scale_matrix(1000, [0, 0, 0]) # origin is [0, 0, 0]
+        #out_mesh.apply_transform(scale_transform)
+        #out_mesh.apply_transform(rot) # Why rotate 180 degrees around x-axis?
         out_mesh.export(mesh_fn)
 
     if visualize:
@@ -592,23 +627,3 @@ def fit_single_frame(img,
 
         img = pil_img.fromarray((output_img * 255).astype(np.uint8))
         img.save(out_img_fn)
-    
-    if save_joints:
-        #Define global orientation
-        global_orient=torch.zeros(1,3, device='cuda:0', requires_grad=True)
-        body_model.reset_params(global_orient=global_orient)
-        model_output = body_model(return_verts=True, body_pose=body_pose)
-        # Joints coordinates
-        joints = model_output.joints.detach().cpu().numpy().squeeze()
-        joints *= 1000 # convert to mm
-        # Rotate joints around x-axis +90 degrees
-        import trimesh
-        rot = trimesh.transformations.rotation_matrix(
-            np.radians(90), [1, 0, 0])
-        joints = trimesh.transform_points(joints, rot)
-        # check if global_orientation is reset
-        #print("body model's orientation: ", body_model.global_orient)
-        # Save joint positions
-        import json
-        with open(joints_path, "w") as f:
-            json.dump(joints.tolist(), f)
